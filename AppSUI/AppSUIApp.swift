0c4a1df994a557ce144c7de1e200eadfd83054f5
Yulia Popova <juliapopova@jivosite.com>
2023-05-31 20:26:16 +0300

    Initial Commit

---
+++ AppSUI/AppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSUIApp.swift
+//  AppSUI
+//
+//  Created by Yulia on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct AppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
